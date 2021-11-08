4febc75cb11f3b2db9c133c715afdfae8c320e40
Dylan Canton <dylcanton@hotmail.com>
2021-11-08 11:17:38 +0100

    Add bdd UML and SQL script

---
+++ SQL-db/amt_pianorgue.sql
@@ -0,0 +1,85 @@
+-- AMT - amt_pianorgue
+-- 08.11.2021
+-- Version: 1.0
+-- Roch, Gianinetti, Canton, Zaccaria, Hungerbühler
+
+-- -----------------------------------------------------
+-- Schema amt_pianorgue
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `amt_pianorgue` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `amt_pianorgue` ;
+
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`article`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`article` (
+  `id` INTEGER UNSIGNED UNIQUE AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL UNIQUE,
+  `price` DECIMAL(10,2) NULL DEFAULT NULL,
+  `description` TEXT NOT NULL,
+  `image` VARCHAR(255) NULL DEFAULT NULL,
+  `stock` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+  
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`category` (
+  `id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`article_category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`article_category` (
+  `articleId` INT UNSIGNED NOT NULL,
+  `categoryId` INT UNSIGNED NOT NULL,
+  CONSTRAINT `article_category_articleId`
+    FOREIGN KEY (`articleId`)
+    REFERENCES `amt_pianorgue`.`article` (`id`),
+  CONSTRAINT `article_category_categoryId`
+    FOREIGN KEY (`categoryId`)
+    REFERENCES `amt_pianorgue`.`category` (`id`)
+) ENGINE=INNODB;
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`user` (
+  `id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`cart`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`cart` (
+  `userId` INT UNSIGNED NOT NULL,
+  `quantity` INT UNSIGNED NOT NULL,
+  `articleId` INT UNSIGNED NOT NULL,
+  CONSTRAINT `cart_articleId`
+    FOREIGN KEY (`articleId`)
+    REFERENCES `amt_pianorgue`.`article` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `cart_userId`
+    FOREIGN KEY (`userId`)
+    REFERENCES `amt_pianorgue`.`user` (`id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+) ENGINE=INNODB;
+
+
+-- -----------------------------------------------------
+-- Table `amt_pianorgue`.`hibernate_sequence`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `amt_pianorgue`.`hibernate_sequence` (
+  `next_val` BIGINT NULL DEFAULT NULL)
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
