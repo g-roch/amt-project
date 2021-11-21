1644784b9aa9052bfa72270f66e8904fb7a02c17
Grimlix <nic.hungi5@gmail.com>
2021-11-21 19:49:49 +0100

    correction ??

--- application/src/main/java/com/amt/app/repository/ArticleRepository.java
+++ application/src/main/java/com/amt/app/repository/ArticleRepository.java
@@ -1,8 +1,12 @@
 package com.amt.app.repository;
 
 import com.amt.app.entities.Article;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Qualifier("articles")
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
 
