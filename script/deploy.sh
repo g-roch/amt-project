6cf747c4889599a8cbd88258d392de53eb4854c8
Gaby Roch <gabriel.roch@heig-vd.ch>
2021-12-06 10:45:17 +0100

    Fixe deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -43,7 +43,7 @@ scp application/target/AMT-Test-*.jar "$HOST":pianorgue
 scp application/src/main/resources/application.properties "$HOST":
 
 ssh "$HOST" sudo systemctl stop pianorgue.service
-ssh "$HOST" sudo mysql pianorgue < SQL-db/SQL-db_Test.sql
+ssh "$HOST" sudo mysql pianorgue < SQL-db/amt_pianorgue.sql
 ssh "$HOST" sudo systemctl start pianorgue.service
 
 # Clean
